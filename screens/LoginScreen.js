b257b33d1f60f220d10a0397339ae9a63c898ee4
TonyCHS <92799152+TonyCHS@users.noreply.github.com>
2022-01-15 15:02:22 +0800

    commit number 1

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,88 @@
-import React from 'react'
-import { StyleSheet, Text, View } from "react-native";
-
-const LoginScreen = () => {
-	return (
-		<View>
-			<Text>LoginScreen</Text>
-		</View>
-	);
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import firebase from "../database/firebaseDB";
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Chat App</Text>
+        <Text style={styles.fieldTitle}>Email</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+          value={email}
+          onChangeText={(input) => setEmail(input)}
+        />
+        <Text style={styles.fieldTitle}>Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="password"
+          autoCorrect={false}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+        />
+        <TouchableOpacity onPress={null} style={styles.loginButton}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
-
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  errorText: {
+    color: "red",
+    height: 40,
+  },
+});
